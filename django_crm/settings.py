daf5fb2b9c94b8d0731d775292da7aa3a088cced
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-09-26 15:21:14 +0545

    added more allowed hosts

--- django_crm/settings.py
+++ django_crm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mydjangocrm.herokuapp.com']
+ALLOWED_HOSTS = ['mydjangocrm.herokuapp.com', 'http://127.0.0.1:8000/', '127.0.0.1']
 
 
 # Application definition
